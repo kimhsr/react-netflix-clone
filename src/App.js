8dc4f2567f94709fd9cdf864177b3f5cc48083c7
kimhsr <kimhsr@naver.com>
2022-12-10 00:14:29 +0900

    이미지 배너 생성

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import React from "react";
 import Nav from "./components/Nav";
+import Banner from "./components/Banner";
 
 function App() {
   return (
     <div className="App">
       <Nav />
+      <Banner />
     </div>
   );
 }
--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "https://themoviedb.org/3",
+  baseURL: "https://api.themoviedb.org/3",
   params: {
     api_key: "713e3ad20466606800bd3464ca5d4a4e",
     language: "ko-KR",
