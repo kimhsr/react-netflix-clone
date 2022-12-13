9bc9420b37376cdf0559e2cdf49594126b0db71a
kimhsr <kimhsr@naver.com>
2022-12-13 15:16:50 +0900

    useLocation 이용 검색 페이지 구현

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,8 +1,11 @@
 import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import "./Nav.css";
 
 export default function Nav() {
   const [show, setShow] = useState(false);
+  const [searchValue, setSearchValue] = useState("");
+  const navigate = useNavigate();
 
   useEffect(() => {
     window.addEventListener("scroll", () => {
@@ -18,18 +21,32 @@ export default function Nav() {
     };
   }, []);
 
+  const handleChange = (e) => {
+    setSearchValue(e.target.value);
+    navigate(`/search?q=${e.target.value}`);
+  };
+
   return (
     <nav className={`nav ${show && "nav_black"}`}>
       <img
         alt="Netflix logo"
         src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/799px-Netflix_2015_logo.svg.png?20190206123158"
-        className="nav_logo"
-        onClick={() => window.location.reload()}
+        className="nav__logo"
+        onClick={() => navigate("/")}
       />
+
+      <input
+        value={searchValue}
+        onChange={handleChange}
+        className="nav__input"
+        type="text"
+        placeholder="영화를 검색해주세요.(영어)"
+      />
+
       <img
         alt="User logged"
         src="https://pbs.twimg.com/media/DgNr961WAAEBGu5?format=jpg&name=small"
-        className="nav_avatar"
+        className="nav__avatar"
       />
     </nav>
   );
